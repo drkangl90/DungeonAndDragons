ced628ce2563c9dc942c6b5b772a8d31bbe870bd
brobohn <brobohn@vt.edu>
2013-11-15 18:27:57 -0500

    Added the MapScreen

---
+++ src/backend/util/MapScreen.java
@@ -0,0 +1,17 @@
+package backend.util;
+
+// -------------------------------------------------------------------------
+/**
+ *  Write a one-sentence summary of your class here.
+ *  Follow it with additional details about its purpose, what abstraction
+ *  it represents, and how to use it.
+ *
+ *  @author Ben
+ *  @version Nov 15, 2013
+ */
+
+public class MapScreen
+    extends sofia.app.ShapeScreen
+{
+
+}
