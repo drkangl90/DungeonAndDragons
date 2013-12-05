b80ebf20eb20b321a7a472a00f85d612d4b40a02
Mary-Wynn Rogers <DrkAngl90@gmail.com>
2013-12-04 23:48:59 -0500

    GUI and directions

--- src/backend/util/Character.java
+++ src/backend/util/Character.java
@@ -9,11 +9,12 @@ import sofia.util.Random;
  * maze. Will compensate for level of map screen.
  *
  * @author Alycia Rouffa (arouffa)
+ * @author Mary-Wynn Rogers (marywynn)
  * @version 11.15.2013
  */
 public class Character
 {
-<<<<<<< Updated upstream
+
     // full health
     private int             baseHealth;
     private int             totalHealth;
@@ -23,7 +24,32 @@ public class Character
 
     private int             level;
     private Random          rand;
-    private directionOfChar leDirection;
+    private Location  direction;
+    protected int     x;
+    protected int     y;
+    private State status;
+    /**
+     * The enum types available.
+     */
+    public enum State
+    {
+        /**
+         * This faces the character to the north direction.
+         */
+        NORTH,
+        /**
+         * This faces the character to the east direction.
+         */
+        EAST,
+        /**
+         * This faces the character to the south direction.
+         */
+        SOUTH,
+        /**
+         * This faces the character to the west direction.
+         */
+        WEST
+    }
 
 
     // Constructor ----------------------------------------------------------
@@ -149,160 +175,194 @@ public class Character
             totalStrength += totalStrength * .1;
         }
     }
-=======
- // full health
- private int             healthField;
- // full capacity of attack
- private int             strengthField;
- private Character       fighter;
- private Character       rogue;
- private Character       wizard;
- private Random          rand;
- private directionOfChar leDirection;
 
 
- // Constructor ----------------------------------------------------------
- /**
-  * Create a new Character object.
-  *
-  * @param health
-  *            The health of the character
-  * @param strength
-  *            The strength of the character.
-  */
- public Character(int health, int strength)
- {
-     // creates the random number generator to be used in following methods.
-     rand = new Random();
-     rand.nextInt(3);
-
-     healthField = health;
-     strengthField = strength;
- }
+    public int x()
+    {
+        return x;
+    }
 
 
- /**
-  * This is the attack method of the character. It will return a random
-  * integer that will then be called from the damage() method in the Map
-  * class on the Monsters
-  *
-  * @return returns the integer value of the attack to the Monsters
-  */
- public int attack()
- {
-     if (rand.nextInt() == 1)
-     {
-         strengthField = 0;
-         return strengthField;
-     }
-     else if (rand.nextInt() == 2)
-     {
-         strengthField /= 2;
-         return strengthField;
-     }
-     else
-     {
-         return strengthField;
-     }
- }
+    // ----------------------------------------------------------
+    /**
+     * gets the direction of the character in the map
+     *
+     * @return returns the location of the character
+     */
+    public Location getDirection()
+    {
+        return new Location(x, y);
+    }
 
 
- /**
-  * This is the getHeatlh() method, which gets the health based on the
-  * character selected.
-  *
-  * @return returns the amount of health the character selected has
-  */
- public int getHealth()
- {
-     return healthField;
- }
+    // ----------------------------------------------------------
+    /**
+     * sets the location of the character
+     * @param direction
+     */
+    public void setDirection(Location direction)
+    {
+        if (State = NORTH)
+        {
+            this.turn(90);
+        }
+    }
 
-
- /**
-  * This is the setHealth() method, which sets the health based on the
-  * character selected.
-  *
-  * @param character
-  *            The character that was selected decides the stats.
-  */
- public void setHealth(Character character)
- {
-     // attack and strength arbitrarily chosen.
-     if (character == fighter)
-     {
-         healthField = 100;
-     }
-     else if (character == rogue)
-     {
-         healthField = 90;
-     }
-     else
-     {
-         healthField = 80;
-     }
- }
-
-
- /**
-  * This is the getStrength() method, which gets the strength based on the
-  * character selected.
-  *
-  * @return returns the amount of strength for attacks that the character
-  *         selected has
-  */
- public int getStrength()
- {
-     return strengthField;
- }
-
-
- /**
-  * This is the setStrength() method which sets the strength based on the
-  * character selected.
-  *
-  * @param character
-  *            The character that was selected decides the stats.
-  */
- public void setStrength(Character character)
- {
-     // attack and strength arbitrarily chosen.
-     if (character == fighter)
-     {
-         strengthField = 10;
-     }
-     else if (character == rogue)
-     {
-         strengthField = 15;
-     }
-     else
-     {
-         strengthField = 20;
-     }
- }
-
-
- /**
-  * The enum types available.
-  */
- public enum directionOfChar
- {
-     /**
-      * This faces the character to the north direction.
-      */
-     NORTH,
-     /**
-      * This faces the character to the east direction.
-      */
-     EAST,
-     /**
-      * This faces the character to the south direction.
-      */
-     SOUTH,
-     /**
-      * This faces the character to the west direction.
-      */
-     WEST
- }
->>>>>>> Stashed changes
-}
+//
+//=======
+// // full health
+// private int             healthField;
+// // full capacity of attack
+// private int             strengthField;
+// private Character       fighter;
+// private Character       rogue;
+// private Character       wizard;
+// private Random          rand;
+// private directionOfChar leDirection;
+//
+//
+// // Constructor ----------------------------------------------------------
+// /**
+//  * Create a new Character object.
+//  *
+//  * @param health
+//  *            The health of the character
+//  * @param strength
+//  *            The strength of the character.
+//  */
+// public Character(int health, int strength)
+// {
+//     // creates the random number generator to be used in following methods.
+//     rand = new Random();
+//     rand.nextInt(3);
+//
+//     healthField = health;
+//     strengthField = strength;
+// }
+//
+//
+// /**
+//  * This is the attack method of the character. It will return a random
+//  * integer that will then be called from the damage() method in the Map
+//  * class on the Monsters
+//  *
+//  * @return returns the integer value of the attack to the Monsters
+//  */
+// public int attack()
+// {
+//     if (rand.nextInt() == 1)
+//     {
+//         strengthField = 0;
+//         return strengthField;
+//     }
+//     else if (rand.nextInt() == 2)
+//     {
+//         strengthField /= 2;
+//         return strengthField;
+//     }
+//     else
+//     {
+//         return strengthField;
+//     }
+// }
+//
+//
+// /**
+//  * This is the getHeatlh() method, which gets the health based on the
+//  * character selected.
+//  *
+//  * @return returns the amount of health the character selected has
+//  */
+// public int getHealth()
+// {
+//     return healthField;
+// }
+//
+//
+// /**
+//  * This is the setHealth() method, which sets the health based on the
+//  * character selected.
+//  *
+//  * @param character
+//  *            The character that was selected decides the stats.
+//  */
+// public void setHealth(Character character)
+// {
+//     // attack and strength arbitrarily chosen.
+//     if (character == fighter)
+//     {
+//         healthField = 100;
+//     }
+//     else if (character == rogue)
+//     {
+//         healthField = 90;
+//     }
+//     else
+//     {
+//         healthField = 80;
+//     }
+// }
+//
+//
+// /**
+//  * This is the getStrength() method, which gets the strength based on the
+//  * character selected.
+//  *
+//  * @return returns the amount of strength for attacks that the character
+//  *         selected has
+//  */
+// public int getStrength()
+// {
+//     return strengthField;
+// }
+//
+//
+// /**
+//  * This is the setStrength() method which sets the strength based on the
+//  * character selected.
+//  *
+//  * @param character
+//  *            The character that was selected decides the stats.
+//  */
+// public void setStrength(Character character)
+// {
+//     // attack and strength arbitrarily chosen.
+//     if (character == fighter)
+//     {
+//         strengthField = 10;
+//     }
+//     else if (character == rogue)
+//     {
+//         strengthField = 15;
+//     }
+//     else
+//     {
+//         strengthField = 20;
+//     }
+// }
+//
+//
+// /**
+//  * The enum types available.
+//  */
+// public enum directionOfChar
+// {
+//     /**
+//      * This faces the character to the north direction.
+//      */
+//     NORTH,
+//     /**
+//      * This faces the character to the east direction.
+//      */
+//     EAST,
+//     /**
+//      * This faces the character to the south direction.
+//      */
+//     SOUTH,
+//     /**
+//      * This faces the character to the west direction.
+//      */
+//     WEST
+// }
+//>>>>>>> Stashed changes
+//}
--- src/backend/util/MapScreen.java
+++ src/backend/util/MapScreen.java
@@ -1,31 +1,64 @@
 package backend.util;
 
+import android.widget.ProgressBar;
+import android.widget.Button;
 import sofia.graphics.RectangleShape;
+import sofia.app.ShapeScreen;
 import sofia.graphics.Color;
-//import android.graphics.Color;
 
 // -------------------------------------------------------------------------
 /**
- *  Write a one-sentence summary of your class here.
- *  Follow it with additional details about its purpose, what abstraction
- *  it represents, and how to use it.
+ * Write a one-sentence summary of your class here. Follow it with additional
+ * details about its purpose, what abstraction it represents, and how to use it.
  *
- *  @author Ben
- *  @version Nov 15, 2013
+ * @author Ben
+ * @author Mary-Wynn Rogers (marywynn)
+ * @version Nov 15, 2013
  */
 
 public class MapScreen
-    extends sofia.app.ShapeScreen
+    extends ShapeScreen
 {
-    private Map map;
+    private Map                map;
     private RectangleShape[][] mapArray;
-    private float side;
-    private boolean hasBeenClicked = false; //for testing
+    private float              side;
+    private boolean            hasBeenClicked = false; // for testing
+    private Button             north;
+    private Button             south;
+    private Button             east;
+    private Button             west;
+    private Button             fight;
+    private Button             defend;
+    private Button             flee;
+    private ProgressBar        health;
+    private State              status;
+
+
+    public enum State
+    {
+        /**
+         * This faces the character to the north direction.
+         */
+        NORTH,
+        /**
+         * This faces the character to the east direction.
+         */
+        EAST,
+        /**
+         * This faces the character to the south direction.
+         */
+        SOUTH,
+        /**
+         * This faces the character to the west direction.
+         */
+        WEST
+    }
+
 
     public void initialize()
     {
         map = new Map(8);
-        mapArray  = new RectangleShape[8][8];
+        mapArray = new RectangleShape[8][8];
         side = Math.min(getWidth(), getHeight());
         side /= 8;
         for (int i = 0; i < 8; i++)
@@ -46,9 +79,11 @@ public class MapScreen
         }
     }
 
+
     // ----------------------------------------------------------
     /**
      * Place a description of your method here.
+     *
      * @param x
      * @param y
      */
@@ -56,6 +91,7 @@ public class MapScreen
     {
         setHasBeenClicked(true);
     }
+
 
     // ----------------------------------------------------------
     /**
@@ -66,13 +102,50 @@ public class MapScreen
         return hasBeenClicked;
     }
 
+
     // ----------------------------------------------------------
     /**
-     * @param hasBeenClicked the hasBeenClicked to set
+     * @param hasBeenClicked
+     *            the hasBeenClicked to set
      */
     public void setHasBeenClicked(boolean hasBeenClicked)
     {
         this.hasBeenClicked = true;
     }
 
+
+    /**
+     * moves the character north
+     */
+    public void northClicked()
+    {
+        // character moves north
+    }
+
+
+    /**
+     * moves the character south
+     */
+    public void southClicked()
+    {
+        // character moves south
+    }
+
+
+    /**
+     * moves the character east
+     */
+    public void eastClicked()
+    {
+        // character moves east
+    }
+
+
+    /**
+     * moves the character west
+     */
+    public void westClicked()
+    {
+        // character moves west
+    }
 }
